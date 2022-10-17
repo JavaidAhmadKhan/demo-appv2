e486f8d9036ba8ecd8152f60d2e29a5e1d3ff212
JavaidAhmadKhan <relaxjavaid@gmail.com>
2022-10-17 23:28:16 +0530

    added react bootsrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,87 @@
+import React, { useState } from "react";
+import { Accordion, Card, Col, Container, Row } from "react-bootstrap";
+
+import Alert from "react-bootstrap/Alert";
+import Button from "react-bootstrap/Button";
+
 import "./App.css";
-import Card from "./Card";
-import contacts from "./contacts";
 
-const createCard = ({ id, title, email, imgURL, phone }) => {
-  return <Card key={id} name={title} img={imgURL} tel={phone} email={email} />;
-};
+export default function App() {
+  const [show, setShow] = useState(true);
 
-function App() {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
-      {contacts.map(createCard)}
+      <Alert show={show} variant="success">
+        <Alert.Heading>How's it going?!</Alert.Heading>
+        <p>
+          Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+          lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
+          fermentum.
+        </p>
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Button onClick={() => setShow(false)} variant="outline-success">
+            Close me y'all!
+          </Button>
+        </div>
+      </Alert>
+
+      {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
+
+      <Container>
+        <Row>
+          <Col sm={8}>
+            The Supplier Code of Conduct applies to all ‘Suppliers globally.
+            ‘Supplier’ here refers to suppliers/service providers/
+            traders/agents/ consultants/ contractors/ joint venture partners/
+            third parties including their employees, agents, and other
+            representatives, who have a business relationship with and provide,
+            sell, seek to sell, any kinds of goods or services to Ayana
+            Renewable Power Private Limited or any of its subsidiaries,
+            affiliates, divisions (“Ayana”). This Supplier Code of Conduct sets
+            forth the requirements that Ayana asks its Suppliers to respect and
+            adhere to when conducting business with or on behalf of Ayana. This
+            Supplier Code of Conduct embodies Ayana’s commitment to
+            internationally recognized standards and applicable statutory
+            requirements concerning Anti-Bribery, Anti-Corruption, Environment
+            Protection, Minimum Wages, Child Labour, Health and Safety,
+            whichever requirements impose the highest standards of conduct.
+          </Col>
+          <Col xs lg="3">
+            <img
+              src="https://ayana-powerv2.vercel.app/static/media/vendor_code_conduct.6d609271df2acffb47eb.png"
+              className="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}"
+              alt=""
+            />
+          </Col>
+        </Row>
+      </Container>
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 }
-
-export default App;
--- src/johndata.js
+++
@@ -1,5 +0,0 @@
-const name = [
-    {
-        name: "javaid",
-    }
-]
